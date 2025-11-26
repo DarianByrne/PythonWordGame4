0c54bdb27d234b546799f64eeb61745e67dc85ec
DarianByrne <173167730+DarianByrne@users.noreply.github.com>
2025-11-26 16:36:22 +0000

    Starting point

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+import model
+
+app = Flask(__name__)
+
+@app.get("/")
+def opening_page():
+    data = model.get_log_data()
+    return render_template(
+        "pattern.html",
+        the_title="Welcome to Xword on the Web!",
+        data_table=data
+    )
+
+@app.get("/byorder/<what>")
+def byorder(what):
+    data = model.get_log_data(what)
+    return render_template(
+        "ordered.html",
+        data_table=data
+    )
+
+@app.post("/processpattern")
+def get_the_results():
+    pat = request.form["pattern"]
+    results = xword.find_possible_matches(pat)
+
+    model.add_to_database(pat, len(results))
+
+    return ", ".join(results)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
---
+++ model.py
@@ -0,0 +1,39 @@
+import DBcm
+
+import platform
+
+if "aws" in platform.uname().release:
+    creds = {
+        "user": "C00296036",
+        "password": "wordgame4passwd",
+        "host": "C00296036.mysql.pythonanywhere-services.com",
+        "database": "C00296036$default",
+    }
+else:
+    creds = {
+        "user": "wordgame4user",
+        "password": "wordgame4passwd",
+        "host": "localhost",
+        "database": "wordgame4DB",
+    }
+
+def add_to_database(t, w, s, m):
+    SQL = """
+        insert into leaderboard
+          (time, who, sourceword, matches)
+        values
+            (%s, %s, %s, %s)
+    """
+    with DBcm.UseDatabase(creds) as db:
+        db.execute(SQL, (t, w, s, m))
+
+def get_leaderboard_data():
+    SQL = f"""
+        select *
+        from leaderboard
+        order by time asc
+    """
+    with DBcm.UseDatabase(creds) as db:
+        db.execute(SQL)
+        res = db.fetchall()
+    return res
